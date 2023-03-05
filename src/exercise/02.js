dc28e211e091dbfad441b0ec2e77307eb3a8b3aa
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2023-03-05 07:31:33 +0100

    ex2(ec2): put getItems in a web worker to further speed it up
    - By putting getItems in a web worker we´re making it asynchronous
      - With a web worker we can put our heavy js code into a separate
        thread so that main thread in the browser is freed up to display
        stuff to the user more rapidly
      - This means that we no longer have to wait for it to finish to do
        other stuff
      - We also have to handle the fact that it is async, so we made use of
        our useAsync hook

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,8 +3,8 @@
 
 import * as React from 'react'
 import {useCombobox} from '../use-combobox'
-import {getItems} from '../filter-cities'
-import {useForceRerender} from '../utils'
+import {getItems} from '../workerized-filter-cities'
+import {useAsync, useForceRerender} from '../utils'
 
 function Menu({
   items,
@@ -59,10 +59,13 @@ function ListItem({
 function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
+  const {data: allItems, run} = useAsync({data: [], status: 'pending'})
 
-  const allItems = React.useMemo(() => getItems(inputValue), [inputValue])
+  React.useEffect(() => {
+    run(getItems(inputValue))
+  }, [inputValue, run])
+
   const items = allItems.slice(0, 100)
-
   const {
     selectedItem,
     highlightedIndex,
